cc5caed0a38b1d511575e207b39019f339554af8
Koumei Mikuni <komei.mikuni@topgate.co.jp>
2020-10-11 19:00:46 +0900

    level out

--- main.go
+++ main.go
@@ -4,20 +4,48 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-chi/chi"
 	"github.com/rs/zerolog"
 )
 
+type LevelHook struct{}
+
+var _ zerolog.Hook = (*LevelHook)(nil)
+
+func (l LevelHook) Run(e *zerolog.Event, level zerolog.Level, _ string) {
+	var levelName string
+
+	switch level {
+	case zerolog.DebugLevel:
+		levelName = "Debug"
+	case zerolog.WarnLevel:
+		levelName = "Warning"
+	case zerolog.ErrorLevel:
+		levelName = "Error"
+	default:
+		levelName = "Info"
+	}
+	e.Str("severity", levelName)
+}
+
 func main() {
-	zerolog.TimestampFieldName = "timestamp"
-	zerolog.LevelFieldName = "survey"
-	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	zerolog.TimestampFieldName = "TimeStamp"
+	zerolog.LevelFieldName = "severity"
+
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger().Hook(LevelHook{})
 	r := chi.NewRouter()
 
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			l := logger.With().Interface("header", r.Header).Logger()
+			trace := r.Header.Get("X-Cloud-Trace-Context")
+			splits := strings.Split(trace, "/")
+			l := logger.With().Logger()
+			if len(splits) > 2 {
+				l = l.With().Str("trace", "project/buld-pack-test/traces/"+splits[0]).Str("span", splits[1]).Logger()
+			}
+
 			r = r.WithContext(l.WithContext(r.Context()))
 			next.ServeHTTP(w, r)
 		})
